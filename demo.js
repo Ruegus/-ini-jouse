230abad973801c2dff44b325cda4566c079bdae3
Ruegus <101264284+Ruegus@users.noreply.github.com>
2022-02-26 09:27:28 +0000

    ASDF

--- demo.js
+++ demo.js
@@ -1,2 +1,8 @@
 
 // demo.js
+
+// This is used by demo.html to demonstrate parseq.js. It includes a widget
+// function that represents a service factory, a show callback that displays the
+// final result, and a parseq routine written as an annotated nested array.
+
+// This interacts with the browser using Plain Old DOM.
