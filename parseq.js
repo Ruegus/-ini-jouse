c49b3fa3dbedcc313c8d3ded6c4774e0599cbb0d
Ruegus <101264284+Ruegus@users.noreply.github.com>
2022-05-27 16:53:39 +0000

    Clang warning.

--- parseq.js
+++ parseq.js
@@ -150,3 +150,5 @@ function run(
 
                         if (
                             cancel_array !== undefined
+                            && number !== undefined
+                        ) {
