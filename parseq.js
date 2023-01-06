1ca31498afa25eefa11ca6408283635d8f83e6e9
Ruegus <101264284+Ruegus@users.noreply.github.com>
2023-01-06 22:34:17 +0000

    Fix #160

--- parseq.js
+++ parseq.js
@@ -532,3 +532,4 @@ function race(requestor_array, time_limit, throttle) {
 
                     cancel(reason);
                     callback(undefined, reason);
+                    callback = undefined;
